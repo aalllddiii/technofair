c3578e37bb8b836a7302b161fe86a362a3384fe7
aalllddiii <aldicahyar@gmail.com>
2024-09-17 10:50:25 +0700

    first commit

---
+++ config/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "bemfikti_technofair";
+
+$con = mysqli_connect($host, $user, $pass, $db) or die("koneksi gagal");
+
+// if ($con) {
+//     echo "ada koneksi";
+// }
